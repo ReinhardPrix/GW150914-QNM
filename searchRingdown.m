836cdd7b0969c6c3c94d8345fd6df962da319d9b
Reinhard Prix <reinhard.prix@ligo.org>
2016-01-15 16:56:22 +0100

    re-instated old time-domain approx for comparison

--- searchRingdown.m
+++ searchRingdown.m
@@ -62,8 +62,8 @@ function ret = searchRingdown ( varargin )
   [ff0, ttau] = meshgrid ( f0, tau );
   lap_s = 1./ ttau + I * 2*pi * ff0;	%% laplace 'frequency'
   Ntempl = length ( lap_s(:) );
-  log10BSG_tot = match_tot = Gam_tot = zeros ( size ( lap_s ) );
-  Is = Ic = Isc = det_gam = normBSG = zeros ( size ( lap_s ) );
+  log10BSG_old = sqrtDetGam = zeros ( size ( lap_s ) );
+  I_s = I_c = I_sc = det_gam = normBSG = match = zeros ( size ( lap_s ) );
   gam_ss = gam_cc = gam_sc = zeros ( size ( lap_s ) );
   DebugPrintf ( 1, "Searching ringdown %d templates ... ", length(ff0(:)) );
 
@@ -71,8 +71,7 @@ function ret = searchRingdown ( varargin )
   t0 = uvar.tCenter + uvar.tOffs;   	%% start-time of exponential ringdown-template
 
   for X = 1:Ndet
-    log10BSG{X} = match{X} = Gam{X} = zeros ( size ( lap_s ) );
-
+    log10BSGX_old{X} = matchX{X} = sqrtDetGamX{X} = zeros ( size ( lap_s ) );
     %% prepare per-detector timeseries for matching: adapt L1 data to be phase-coherent with H1
     if ( strcmp ( ts{X}.IFO, "L1" ) )
       shiftL1_eff = round ( shiftL1 / dt ) * dt;
@@ -95,25 +94,20 @@ function ret = searchRingdown ( varargin )
   inds_mat = xx + yy;
   Stot_k  = mean ( Stot ( inds_mat ), 1 );
   Stot_mat = meshgrid ( Stot_k, ones(1,Ntau) );
-  Gam_tot = ( ttau ./ Stot_mat  + 1 / uvar.prior_H^2 ).^(-1);
-  for X = 1:Ndet
-    Sn_k{X} = mean ( psd{X}.Sn ( inds_mat ), 1 );
-    Sn_mat{X} = meshgrid ( Sn_k{X}, ones(1,Ntau) );
-    Gam{X} = ( ttau ./ Sn_mat{X} + 1 / uvar.prior_H^2 ).^(-1);
-  endfor
+  I_oo_old = ttau ./ Stot_mat;
 
   %% ---------- search parameter-space in {f0, tau} and compute matched-filter in each template ----------
   for l = 1 : Ntempl	%% loop over all templates
     %% ----- (complex) time-domain template
     hExp_i = exp ( - Dt_i{1}  * lap_s(l) );
     for X = 1:Ndet
-      match{X}(l) = 2 * dt * sum ( xOW_i{X} .* hExp_i );
+      matchX{X}(l) = 2 * dt * sum ( xOW_i{X} .* hExp_i );
     endfor %% X
   endfor
   for X = 1:Ndet
-    match_tot   += match{X};
-    x_s = - imag ( match_tot );
-    x_c =   real ( match_tot );
+    match     +=   matchX{X};
+    x_s        = - imag ( match );
+    x_c        =   real ( match );
   endfor
 
   for l = 1 : Ntempl
@@ -122,41 +116,35 @@ function ret = searchRingdown ( varargin )
     hsFT_k  = ttau(l) * ( 2*pi * ff0(l) * ttau(l) ) ./ denom_k;
     hcFT_k  = ttau(l) * ( 1 + I * 2*pi*fk * ttau(l) ) ./ denom_k;
     %% ----- compute M-matrix from template self-match integrals in frequency-domain ----------
-    Is(l)  = 4 * Ndet * df * sum ( abs(hsFT_k).^2 );
-    Ic(l)  = 4 * Ndet * df * sum ( abs(hcFT_k).^2 );
-    Isc(l) = 4 * Ndet * df * real ( sum ( hsFT_k .* conj(hcFT_k) ) );
+    I_s(l)  = 4 * Ndet * df * sum ( abs(hsFT_k).^2 );
+    I_c(l)  = 4 * Ndet * df * sum ( abs(hcFT_k).^2 );
+    I_sc(l) = 4 * Ndet * df * real ( sum ( hsFT_k .* conj(hcFT_k) ) );
   endfor %% l
-  detM = Is .* Ic - Isc.^2;
-  trM  = Is + Ic;
+  detM = I_s .* I_c - I_sc.^2;
+  trM  = I_s + I_c;
   Hm2 = uvar.prior_H^(-2);
-  det_gamInv = ( Is + Hm2 ) .* ( Ic + Hm2 ) - Isc.^2;
+
+  det_gamInv = ( I_s + Hm2 ) .* ( I_c + Hm2 ) - I_sc.^2;
   det_gam = 1./ det_gamInv;
   normBSG = sqrt(det_gam) * Hm2;
-  gam_ss  = det_gam .* ( Ic + Hm2 );
-  gam_cc  = det_gam .* ( Is + Hm2 );
-  gam_sc  = det_gam .* ( -Isc );
 
+  gam_ss  = det_gam .* ( I_c + Hm2 );
+  gam_cc  = det_gam .* ( I_s + Hm2 );
+  gam_sc  = det_gam .* ( -I_sc );
   x_gam_x = gam_ss .* x_s.^2 + 2 * gam_sc .* x_s .* x_c + gam_cc .* x_c.^2;
-  BSGalt = normBSG .* exp ( 0.5 .* x_gam_x );
-  for X = 1:Ndet
-    log10BSG{X} = compute_log10BSG ( Gam{X}, match{X}, uvar.prior_H );
-    BSG{X} = 10.^(log10BSG{X});
-  endfor
-  log10BSG_tot = compute_log10BSG ( Gam_tot, match_tot, uvar.prior_H );
-  BSG_tot = 10.^log10BSG_tot;
-
-
-  posterior = struct ( "f0", ff0, "tau", ttau, "BSG", BSG_tot );
+  BSG = normBSG .* exp ( 0.5 .* x_gam_x );
+  posterior = struct ( "f0", ff0, "tau", ttau, "BSG", BSG );
   DebugPrintf ( 1, "done.\n");
 
-  DebugPrintf (2,  "<Gam> = %g, <Gam{1}> = %g, <Gam{2}> = %g\n", mean(Gam_tot(:)), mean(Gam{1}(:)), mean(Gam{2}(:)) );
-  DebugPrintf (2,  "max|match| = %g, max|match{1}| = %g, max|match{2}| = %g\n", max(abs(match_tot(:))), max(abs(match{1}(:))), max(abs(match{2}(:))) );
-  DebugPrintf (2,  "max[log10BSG] = %g, max[log10BSG{1}] = %g, max[log10BSG{2}] = %g\n", max(log10BSG_tot(:)), max(log10BSG{1}(:)), max(log10BSG{2}(:)) );
+  %% ----- 'old' way ----------
+  gam_oo_old = ( I_oo_old + Hm2 ) .^ (-1);	%% diagonal matrix, inverse
+  det_gam_old = gam_oo_old .^ 2;
+  normBSG_old = sqrt(det_gam_old) * Hm2;
+  x_gam_x_old = (x_s.^2 + x_c.^2) .* gam_oo_old;
+  BSG_old = normBSG_old .* exp ( 0.5 .* x_gam_x_old );
 
   %% ---------- compute marginalized posteriors on {f,tau} ----------
-  %% renormalize to avoid overflow
-  log10BSG_renorm = log10BSG_tot - max(log10BSG_tot(:));	%% == divide BSGL by max(BSGL) ==> max now at 1
-  BSG_renorm = 10.^log10BSG_renorm;
+  BSG_renorm = BSG / max ( BSG(:) );
   posterior_f0   = sum ( BSG_renorm, 1 );
   norm_f0 = step_f0 * sum ( posterior_f0 );
   posterior_f0 /= norm_f0;
@@ -176,24 +164,21 @@ function ret = searchRingdown ( varargin )
   end_try_catch
 
   %% ---------- estimate SNR for all templates ----------
-  AVec_est = Gam_tot .* match_tot;
-  A_est = abs ( AVec_est );
-  phi0_est = arg ( AVec_est );
-  SNR_est = A_est .* sqrt ( Ndet * ttau ./ (2 * Stot_mat ) );
+  A_s_est = gam_ss .* x_s + gam_sc .* x_c;
+  A_c_est = gam_sc .* x_s + gam_cc .* x_c;
+  A_est = sqrt ( A_s_est.^2 + A_c_est.^2 );
+  phi0_est = - atan2 ( A_s_est, A_c_est );
+  SNR_est = A_s_est.^2 .* I_s + 2 * A_s_est .* I_sc .* A_c_est + A_c_est.^2 .* I_c;
 
   %% ---------- determine maximum-posterior estimates (MPE) ----------
-  BSG_max = max ( BSG_tot(:) );
-  l_MPE = ( find ( BSG_tot(:) == BSG_max ) )(1);
-  Stot_MPE = Stot_mat ( l_MPE );
+  BSG_max = max ( BSG(:) );
+  l_MPE = ( find ( BSG(:) == BSG_max ) )(1);
   f0_MPE = ff0 ( l_MPE );
   tau_MPE = ttau ( l_MPE );
-  Gam_MPE = Gam_tot ( l_MPE );
-  F_MPE = match_tot ( l_MPE );
 
-  A_MPE = A_est(l_MPE);
-  phi0_MPE = phi0_est(l_MPE);
-  SNR_MPE = SNR_est(l_MPE);
-
+  A_MPE    = A_est( l_MPE );
+  phi0_MPE = phi0_est ( l_MPE );
+  SNR_MPE  = SNR_est ( l_MPE );
   %% ---------- Plot results ----------
 
   %% ----- Fig 1: Bayes factor / posterior over {f0,tau} ----------
@@ -202,7 +187,7 @@ function ret = searchRingdown ( varargin )
     subplot ( 2, 2, 1 );
     hold on;
     colormap ("jet");
-    surf ( ff0, ttau * 1e3, BSG_tot ); colorbar("location", "NorthOutside"); view(2); shading("interp");
+    surf ( ff0, ttau * 1e3, BSG ); colorbar("location", "NorthOutside"); view(2); shading("interp");
     plot3 ( f0_MPE, tau_MPE * 1e3, 1.1*BSG_max, "marker", "o", "markersize", 3, "color", "white" );
     yrange = ylim();
     xlabel ("f0 [Hz]"); ylabel ("tau [ms]");
@@ -237,7 +222,7 @@ function ret = searchRingdown ( varargin )
     colors = { "red", "blue" };
     for X = 1:Ndet
       sleg = sprintf (";%s;", ts{X}.IFO );
-      plot ( ts{X}.ti - uvar.tCenter, ts{X}.xiOW * Stot_MPE, sleg, "linewidth", 2, "color", colors{X} );
+      plot ( ts{X}.ti - uvar.tCenter, ts{X}.xi, sleg, "linewidth", 2, "color", colors{X} );
     endfor
     Dt = ts{1}.ti - t0;
     indsRingdown = find ( Dt >= 0 );
@@ -258,31 +243,26 @@ function ret = searchRingdown ( varargin )
     grid on;
     hold off;
 
-    figure(); clf;
-    hold on;
-    colormap ("jet");
-    surf ( ff0, ttau * 1e3, BSGalt ); colorbar("location", "NorthOutside"); view(2); shading("interp");
-    yrange = ylim();
-    xlabel ("f0 [Hz]"); ylabel ("tau [ms]");
-    hold off;
-
   endif %% plotResults
 
+  figure(); clf;
+  hold on;
+  colormap ("jet");
+  surf ( ff0, ttau * 1e3, BSG_old ); colorbar("location", "NorthOutside"); view(2); shading("interp");
+  yrange = ylim();
+  xlabel ("f0 [Hz]"); ylabel ("tau [ms]");
+  hold off;
+
   %% summarize numerical outcomes on stdout
-  BSG_mean = mean ( BSG_tot(:) );
-  BSGalt_mean = mean ( BSGalt(:) );
+  BSG_mean = mean ( BSG(:) );
 
   DebugPrintf (1, "tGPS    = %.0f + %f s\n", uvar.tCenter, uvar.tOffs );
   DebugPrintf (1, "<BSG>   = %.2g\n", BSG_mean );
-  DebugPrintf (1, "<BSGalt>= %.2g\n", BSGalt_mean );
   DebugPrintf (1, "f0_est  = { %.1f, %.1f, %1.f } Hz\n", f0_est.lower, f0_est.MPE, f0_est.upper );
   DebugPrintf (1, "tau_est = { %.1f, %.1f, %1.f } ms\n", 1e3 * tau_est.lower, 1e3 * tau_est.MPE, 1e3 * tau_est.upper );
   DebugPrintf (1, "A_MPE   = %.2g\n", A_MPE );
   DebugPrintf (1, "phi0_MPE= %.2g\n", phi0_MPE );
-  DebugPrintf (1, "sqrt(S)(f_MPE)= %.2g\n", sqrt(Stot_MPE) );
   DebugPrintf (1, "SNR_MPE = %.2g\n", SNR_MPE );
-  DebugPrintf (1, "sanity check: BSG_s = Lr_s ~ e^(SNR^2/2) = %.2g\n", exp ( SNR_MPE^2 / 2 ) );
-
 
   ret = struct ( "bname", bname, ...
                  "tGPS", uvar.tCenter + uvar.tOffs, ...
@@ -292,7 +272,6 @@ function ret = searchRingdown ( varargin )
                  "f0_est", f0_est, ...
                  "tau_est", tau_est, ...
                  "SNR_MPE", SNR_MPE, ...
-                 "sqrtS_MPE", sqrt(Stot_MPE), ...
                  "posterior", posterior, ...
                  "SNR", SNR_est
                );
@@ -301,9 +280,9 @@ function ret = searchRingdown ( varargin )
 
 endfunction
 
-function log10BSG = compute_log10BSG ( Gam, match, H )
-  Fstat = 0.5 * Gam .* abs ( match ).^2;
-  log10BSG = log10(Gam) - 2 * log10(H) + Fstat * log10(e);
+function log10BSG = compute_log10BSG ( sqrtDetGam, match, H )
+  Fstat = 0.5 * sqrtDetGam .* abs ( match ).^2;
+  log10BSG = log10(sqrtDetGam) - 2 * log10(H) + Fstat * log10(e);
   return;
 endfunction
 
