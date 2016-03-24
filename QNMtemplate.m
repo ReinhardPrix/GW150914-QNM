82ee29b3aa6b9ad9321b9900db409d281b30d682
Reinhard Prix <reinhard.prix@ligo.org>
2016-03-24 11:37:11 +0100

    QNMtemplate(): produce one-sided or symmetric template
    - symmetric template needed for injections, to avoid
      discontinuity-artifacts in the data

--- QNMtemplate.m
+++ QNMtemplate.m
@@ -1,17 +1,27 @@
-function tsQNM = QNMtemplate ( t0GPS, A, phi0, f0, tau, ts )
+function tsQNM = QNMtemplate ( t0GPS, A, phi0, f0, tau, ts, symmetric = false )
   ## return QNM template time-series of params {t0GPS, A, phi0, f0, tau} on given time-series 'ts'
-  xi = [];
+
+  tsQNM = ts;
+  tsQNM.xi(:) = 0;
 
   Dt_i = ts.ti - (t0GPS - ts.epoch);
   indsRingdown = find ( Dt_i >= 0 );
-  if ( isempty ( indsRingdown ) )
+  indsRingup = [];
+  if ( symmetric )
+    indsRingup = find ( Dt_i < 0 );
+  endif
+  if ( isempty ( indsRingdown ) && isempty ( indsRingup ) )
     return;
   endif
 
-  Dt_pos = Dt_i ( indsRingdown );
-  tsQNM.xi = A * e.^(- Dt_pos / tau ) .* cos ( 2*pi * f0 * Dt_pos + phi0 );
-  tsQNM.epoch = t0GPS + min(Dt_pos);
-  tsQNM.ti = Dt_pos - min(Dt_pos);	%% timesteps start from 0
+  if ( !isempty ( indsRingdown ) )
+    Dt_pos = Dt_i ( indsRingdown );
+    tsQNM.xi ( indsRingdown )  = A * e.^(- Dt_pos / tau ) .* cos ( 2*pi * f0 * Dt_pos + phi0 );
+  endif
+  if ( !isempty ( indsRingup ) )
+    Dt_neg = Dt_i ( indsRingup );
+    tsQNM.xi ( indsRingup )  = A * e.^( Dt_neg / tau ) .* cos ( 2*pi * f0 * Dt_neg + phi0 );
+  endif
 
   return;
 
