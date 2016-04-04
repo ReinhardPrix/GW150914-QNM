f7d433863bd59cb31f356cb9187ef31613f65940
Reinhard Prix <reinhard.prix@ligo.org>
2016-04-04 15:51:43 +0200

    DebugPrintSummary: fixed output if 1D intervals failed

--- DebugPrintSummary.m
+++ DebugPrintSummary.m
@@ -3,16 +3,14 @@ function DebugPrintSummary ( level, res )
   %% summarize numerical outcomes on stdout
   DebugPrintf (level, "---------- t0 = tMerger + tOffs = %.6f s + %.1f ms = %.6f s ----------\n", res.tMerger, res.tOffs * 1e3, res.t0 );
   DebugPrintf (level, "log10<BSG>= %.2g\n", log10(res.BSG) );
-  DebugPrintf (level, "f0_est    = {%.1f -%.1f +%.1f} Hz\n", res.f0_est.MPE, res.f0_est.lerr, res.f0_est.uerr );
-  DebugPrintf (level, "tau_est   = {%.1f -%.1f +%1.f} ms\n", 1e3 * [res.tau_est.MPE, res.tau_est.lerr, res.tau_est.uerr] );
-  DebugPrintf (level, "-\n");
-  DebugPrintf (level, "A_MP      = %.2g\n", res.AmpMP.A );
-  DebugPrintf (level, "phi0_MP   = %.2g\n", res.AmpMP.phi0 );
-  DebugPrintf (level, "SNR_MP    = %.2g\n", res.AmpMP.SNR );
-  DebugPrintf (level, "-\n");
-  DebugPrintf (level, "A_ML      = %.2g\n", res.AmpML.A );
-  DebugPrintf (level, "phi0_ML   = %.2g\n", res.AmpML.phi0 );
-  DebugPrintf (level, "SNR_ML    = %.2g\n", res.AmpML.SNR );
-  DebugPrintf (level, "-\n");
+  if ( !isempty ( res.f0_est ) )
+    DebugPrintf (level, "f0_est    = {%.1f -%.1f +%.1f} Hz\n", res.f0_est.MPE, res.f0_est.lerr, res.f0_est.uerr );
+  endif
+  if ( !isempty ( res.tau_est ) )
+    DebugPrintf (level, "tau_est   = {%.1f -%.1f +%1.f} ms\n", 1e3 * [res.tau_est.MPE, res.tau_est.lerr, res.tau_est.uerr] );
+  endif
+  DebugPrintf (level, "A    [MP|ML] = %.2g | %.2g\n", res.AmpMP.A, res.AmpML.A );
+  DebugPrintf (level, "phi0 [MP|ML] = %.2g | %.2g\n", res.AmpMP.phi0, res.AmpML.phi0 );
+  DebugPrintf (level, "SNR  [MP|ML] = %.2g | %.2g\n", res.AmpMP.SNR, res.AmpML.SNR );
   return;
 endfunction
